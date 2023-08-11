733dd384e9adb62373f63bab79eb54602616bffb
rtsoy <tsoiroman98@gmail.com>
2023-08-11 12:49:11 +0600

    feat: add user management client

--- usermgmt_client/usermgmt_client.go
+++ usermgmt_client/usermgmt_client.go
@@ -1,5 +1,50 @@
 package main
 
-func main() {
+import (
+	"context"
+	"log"
+	"time"
 
+	pb "github.com/rtsoy/grpc-test/usermgmt"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+const address = "localhost:50051"
+
+func main() {
+	conn, err := grpc.Dial(address, grpc.WithTransportCredentials(insecure.NewCredentials()), grpc.WithBlock())
+	if err != nil {
+		log.Fatalf("failed to connect: %v", err)
+	}
+	defer conn.Close()
+
+	c := pb.NewUserManagementClient(conn)
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+
+	newUsers := map[string]int32{
+		"Alice": 32,
+		"Mark":  17,
+		"John":  25,
+		"Marry": 20,
+	}
+
+	for name, age := range newUsers {
+		resp, err := c.CreateNewUser(ctx, &pb.NewUser{
+			Name: name,
+			Age:  age,
+		})
+		if err != nil {
+			log.Fatalf("failed to create an user: %v", err)
+		}
+
+		log.Printf(`
+			USER DETAILS:
+			ID: %d
+			Name: %s
+			Age: %d
+        `, resp.GetId(), resp.GetName(), resp.GetAge())
+	}
 }
