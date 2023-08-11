7732ba42c7eb2e8804dd05b62b5c6ce24840768f
rtsoy <tsoiroman98@gmail.com>
2023-08-12 00:13:00 +0600

    feat: add persistence to data with postgres database

--- usermgmt_server/usermgmt_server.go
+++ usermgmt_server/usermgmt_server.go
@@ -5,101 +5,107 @@ import (
 	"errors"
 	"fmt"
 	"log"
-	"math/rand"
 	"net"
 	"os"
 
+	"github.com/jackc/pgx/v5"
+	_ "github.com/joho/godotenv/autoload"
 	pb "github.com/rtsoy/grpc-test/usermgmt"
 	"google.golang.org/grpc"
-	"google.golang.org/protobuf/encoding/protojson"
+	"google.golang.org/grpc/reflection"
 )
 
-const (
-	port     = ":50051"
-	filename = "users.json"
+var (
+	grpcPort = os.Getenv("GRPC_PORT")
+
+	host     = os.Getenv("POSTGRES_HOST")
+	port     = os.Getenv("POSTGRES_PORT")
+	user     = os.Getenv("POSTGRES_USER")
+	password = os.Getenv("POSTGRES_PASSWORD")
+	dbName   = os.Getenv("POSTGRES_DB")
+
+	ctx = context.Background()
 )
 
 type UserManagementServer struct {
+	conn *pgx.Conn
 	pb.UnimplementedUserManagementServer
 }
 
-func NewUserManagementServer() *UserManagementServer {
-	return &UserManagementServer{}
+func NewUserManagementServer(conn *pgx.Conn) *UserManagementServer {
+	return &UserManagementServer{
+		conn: conn,
+	}
 }
 
 func (s *UserManagementServer) CreateNewUser(ctx context.Context, in *pb.NewUser) (*pb.User, error) {
 	log.Printf("Received: %v", in.GetName())
 
+	tx, err := s.conn.Begin(ctx)
+	if err != nil {
+		return nil, fmt.Errorf("failed to start a transaction: %v", err)
+	}
+
+	var userID int32
+	if err := tx.QueryRow(ctx, `
+		INSERT INTO users(name, age)
+		VALUES ($1, $2)
+		RETURNING id
+    `, in.GetName(), in.GetAge()).Scan(&userID); err != nil {
+		return nil, fmt.Errorf("failed to execute a query: %v", err)
+	}
+
+	if err := tx.Commit(ctx); err != nil {
+		return nil, fmt.Errorf("failed to commit a transaction: %v", err)
+	}
+
 	createdUser := &pb.User{
-		Id:   rand.Int31(),
+		Id:   userID,
 		Name: in.GetName(),
 		Age:  in.GetAge(),
-	}
-
-	usersList := &pb.UsersList{}
-
-	readBytes, err := os.ReadFile(filename)
-	if err != nil {
-		if errors.Is(err, os.ErrNotExist) {
-			log.Printf("file `%s` not found. creating a new file...\n", filename)
-
-			usersList.Users = append(usersList.Users, createdUser)
-
-			jsonBytes, err := protojson.Marshal(usersList)
-			if err != nil {
-				log.Fatalf("JSON Marshalling failed: %v", err)
-			}
-
-			if err := os.WriteFile(filename, jsonBytes, 0664); err != nil {
-				log.Fatalf("failed write to file: %v", err)
-			}
-
-			return createdUser, nil
-		}
-
-		log.Fatalf("failed to read a file: %v", err)
-	}
-
-	if err := protojson.Unmarshal(readBytes, usersList); err != nil {
-		log.Fatal("JSON Unmarshalling failed")
-	}
-
-	usersList.Users = append(usersList.Users, createdUser)
-
-	jsonBytes, err := protojson.Marshal(usersList)
-	if err != nil {
-		log.Fatalf("JSON Marshalling failed: %v", err)
-	}
-
-	if err := os.WriteFile(filename, jsonBytes, 0664); err != nil {
-		log.Fatalf("failed write to file: %v", err)
 	}
 
 	return createdUser, nil
 }
 
 func (s *UserManagementServer) GetUsers(ctx context.Context, in *pb.GetUsersParams) (*pb.UsersList, error) {
-	jsonBytes, err := os.ReadFile(filename)
+	rows, err := s.conn.Query(ctx, `
+		SELECT id, name, age
+		FROM users
+    `)
 	if err != nil {
-		log.Fatalf("failed to read from file: %v", err)
+		if errors.Is(err, pgx.ErrNoRows) {
+			return nil, nil
+		}
+
+		return nil, fmt.Errorf("failed to retrieve a data: %v", err)
 	}
+	defer rows.Close()
 
 	usersList := &pb.UsersList{}
-	if err := protojson.Unmarshal(jsonBytes, usersList); err != nil {
-		log.Fatalf("fJSON Unmarshalling failed")
+
+	for rows.Next() {
+		user := &pb.User{}
+		if err := rows.Scan(&user.Id, &user.Name, &user.Age); err != nil {
+			return nil, fmt.Errorf("failed to read a data from db: %v", err)
+		}
+
+		usersList.Users = append(usersList.Users, user)
 	}
 
 	return usersList, nil
 }
 
 func (s *UserManagementServer) Run() error {
-	lis, err := net.Listen("tcp", port)
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%s", grpcPort))
 	if err != nil {
 		return fmt.Errorf("failed to listen: %v", err)
 	}
 
 	server := grpc.NewServer()
+	reflection.Register(server)
 	pb.RegisterUserManagementServer(server, s)
+
 	log.Printf("server is listening at %v", lis.Addr())
 
 	if err := server.Serve(lis); err != nil {
@@ -110,7 +116,15 @@ func (s *UserManagementServer) Run() error {
 }
 
 func main() {
-	server := NewUserManagementServer()
+	connString := fmt.Sprintf("postgres://%s:%s@%s:%s/%s", user, password, host, port, dbName)
+
+	conn, err := pgx.Connect(ctx, connString)
+	if err != nil {
+		log.Fatalf("failed to establish a connection to postgres: %v", err)
+	}
+	defer conn.Close(ctx)
+
+	server := NewUserManagementServer(conn)
 
 	if err := server.Run(); err != nil {
 		log.Fatal(err)
